9e60c8fcac0540af71bb6c30de41f96fc0a004d5
sanadbawab0 <sanadbawab0@gmail.com>
2023-08-08 14:19:36 +0300

    base

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'title' : 'التدوينة الأولى',
+        'content' : 'نص التدوينة الأولى كنص تجريبي',
+        'post_date' : '15-3-2019',
+        'author' : 'Sanad Bawab'
+    },
+    {
+        'title' : 'التدوينة الثانية',
+        'content' : 'نص التدوينة الثانية كنص تجريبي',
+        'post_date' : '20-3-2020',
+        'author' : 'Yanal Esaied'
+    },
+    {
+        'title' : 'التدوينة الثالثة',
+        'content' : 'نص التدوينة الثالثة كنص تجريبي',
+        'post_date' : '28-6-2022',
+        'author' : 'Wajd Bawab'
+    },
+    {
+        'title' : 'التدوينة الرابعة',
+        'content' : 'نص التدوينة الرابعة كنص تجريبي',
+        'post_date' : '12-9-2021',
+        'author' : 'Own Esaied'
+    },
+]
+
 def home(request):
-    context={'title':'home'}
+    context={'title':'الصفحة الرئيسية',
+             "posts": posts}
     return render(request, 'blog/index.html',context)
 
-# Create your views here.
+def about(request):
+    return render(request, 'blog/about.html',{'title':'من انا'})
